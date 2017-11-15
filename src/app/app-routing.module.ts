75877bf99be39a00e23b021b1f6708fde0457b71
juanjo3200 <juanjo3200@gmail.com>
2017-11-15 21:46:18 +0100

    with bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { ErrorComponent } from "./components/error/error.component";
 import { HomeComponent } from "./components/home/home.component";
+import { RegisterComponent } from './components/register/register.component';
 const routes : Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent},
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: ErrorComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatCardModule} from '@angular/material';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -21,11 +20,7 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
